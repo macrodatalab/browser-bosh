a4cc35131988c1efa925da0735e30f1bc144a79a
ethan <ethanfu@macrodatalab.com>
2015-07-23 03:17:12 -0700

    MODIFY: newbosh

--- bosh/bosh_angular.js
+++ bosh/bosh_angular.js
@@ -1,54 +1,5 @@
 ﻿(function(window,undefined){
 
-angular.module('ng-terminal-example', ['vtortola.ng-terminal', 'ng-terminal-example.command.tools', 'ng-terminal-bosh.command'])
-.controller('console',['$scope','commandBroker','$rootScope', function ($scope, commandBroker, $rootScope) {
-
-    $rootScope.theme = 'vintage';
-
-    setTimeout(function () {
-        $scope.$broadcast('terminal-output', {
-            output: true,
-            text: ['Double Click to start using BigObject Shell'
-                   ],
-            breakLine: true
-        });
-        $scope.$apply();
-    }, 100);
-
-
-    $scope.session = {
-        commands: [],
-        output: [],
-        $scope:$scope
-   };
-
-    $scope.$watchCollection(function () { return $scope.session.commands; }, function (n) {
-        for (var i = 0; i < n.length; i++) {
-            $scope.$broadcast('terminal-command', n[i]);
-        }
-        $scope.session.commands.splice(0, $scope.session.commands.length);
-        $scope.$$phase || $scope.$apply();
-    });
-
-    $scope.$watchCollection(function () { return $scope.session.output; }, function (n) {
-        for (var i = 0; i < n.length; i++) {
-            $scope.$broadcast('terminal-output', n[i]);
-        }
-        $scope.session.output.splice(0, $scope.session.output.length);
-        $scope.$$phase || $scope.$apply();
-    });
-
-
-    $scope.$on('terminal-input', function (e, consoleInput) {
-        var cmd = consoleInput[0];
-        try {
-            commandBroker.execute($scope.session, cmd.command);
-        } catch (err) {
-            $scope.session.output.push({ output: true, breakLine: true, text: [err.message] });
-        }
-    });
-}]);
-
 
  var getschema = function(url,$scope){
                   $.get(url)
@@ -102,7 +53,7 @@ angular.module('ng-terminal-example', ['vtortola.ng-terminal', 'ng-terminal-exam
                  });
  }
 var getBourl = function(){
-      return window.bo.target;
+      return window.bosh.target;
 };
 var attrtypes=['STRING','INT','INT32','FLOAT','VARSTRING','DATETIME32','DOUBLE'];
 var schemaAPP = angular.module('schemaAPP', []);
